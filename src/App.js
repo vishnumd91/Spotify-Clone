9192b9b927db1a66fe3efc2fcd18ef946c6b8564
vishnumd91 <vishnudivakaran1@gmail.com>
2020-08-30 18:33:15 +0530

    UI working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
@@ -11,7 +13,7 @@ const spotify = new SpotifyWebApi();
 
 function App() {
 
-  const [{ user,token },dispatch] = useDataLayerValue();
+  const [token,dispatch] = useDataLayerValue();
 
   useEffect( () => {
     const hash= getTokenFromUrl();
@@ -23,17 +25,17 @@ function App() {
         type: 'SET_TOKEN',
         token: _token,
       })
-      spotify.setAccessToken(_token);
+      spotify.setAccessToken(token);
 
       spotify.getMe().then(user => {
-        // console.log('User Name is:',user);
+        console.log('User Name is:',user);
         dispatch({
           type: 'SET_USER',
           user: user,
         });
       });
     }
-    console.log('I have a token =>',token);
+    console.log('I have a token =>',_token);
     
   }, []);
 
@@ -41,7 +43,7 @@ function App() {
     <div className="app">
       {
         token?(
-          <Player></Player>
+          <Player spotify={spotify}></Player>
         ) :
 
         (
--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,8 @@ export const initialState = {
     user: null,
     playlists: [],
     playing: false,
-    item: null
+    item: null,
+    token: 'BQCp97Frt5AX89KGzDWIlCB7uYc_-YPc9HKxo1moM7Zdgch9LJ…_neajwBdFLDD0tiDDWAHahwuoas3KX2GpM5F5wQLKQJDsWHG3',
 }
 
 const reducer = (state,action) => {
